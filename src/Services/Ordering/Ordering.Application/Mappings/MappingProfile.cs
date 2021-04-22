35bf169b186a4d387af4f7d9528883ee567f4fcd
Athar Imam <mohammadatharimam@gmail.com>
2021-04-23 02:19:37 +0530

    Compete Update Order Command

--- src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
 using Ordering.Application.Features.Orders.Queries.GetOrderList;
 using Ordering.Domain.Entities;
 
@@ -11,6 +12,7 @@ namespace Ordering.Application.Mappings
         {
             this.CreateMap<Order, OrdersVm>().ReverseMap();
             this.CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            this.CreateMap<Order, UpdateOrderCommand>().ReverseMap();
         }
     }
 }
